175387bd7485d5f48fe2e3c3ea8f1b545088b6f1
Achinda <achindamihiruk@gmail.com>
2023-08-16 00:28:35 +0530

    Create an array to add customers

--- BankingApp.java
+++ BankingApp.java
@@ -5,7 +5,8 @@ public class BankingApp {
     public static void main(String[] args) {
      
         final String CLEAR = "\033[H\033[2J";
-        final String BLUE_BOLT_START = "\033[34;1m";
+        final String BLUE_BOLD_START = "\033[34;1m";
+        final String RED_BOLD_START = "\033[31;1m";
         final String RESET = "\033[30;0m";
 
         final String MAIN_MENU = "\u1F4B0 Welcome to Smart Banking App";
@@ -19,8 +20,11 @@ public class BankingApp {
 
         String screen = MAIN_MENU;
 
-       // do {
-            final String APP_TITLE = String.format("%s%s%s",BLUE_BOLT_START, screen, RESET);
+        String[] custName = new String[0];
+
+        Loop:
+        do {
+            final String APP_TITLE = String.format("%s%s%s",BLUE_BOLD_START, screen, RESET);
             System.out.println(CLEAR);
             System.out.println("\t" + APP_TITLE + "\n");
             switch (screen) {
@@ -32,15 +36,60 @@ public class BankingApp {
                 System.out.println(" [5]. Check Account Balance");
                 System.out.println(" [6]. Drop Existig Account");
                 System.out.println(" [7]. Exit\n");
-                System.out.print("\tEnter an option to continue: ");
+                System.out.print(" Enter an option to continue: ");
 
                 int option = scanner.nextInt();
                 scanner.nextLine();
+
+                switch (option){
+                    case 1: screen = OPEN_NEW_ACCOUNT; break;
+                    case 2: screen = DEPOSIT_MONEY; break;
+                    case 3: screen = WITHDRAW_MONEY; break;
+                    case 4: screen = TRANSFER_MONEY; break;
+                    case 5: screen = CHECK_ACCOUNT_BALANCE; break;
+                    case 6: screen = DEPOSIT_MONEY; break;
+                    case 7: System.out.println(CLEAR); System.exit(0);
+                    default: continue;
+                }
                     break;
-            
+                case OPEN_NEW_ACCOUNT: 
+                System.out.printf("ID: SDB-%05d \n", (custName.length + 1));
+                
+                boolean valid;
+                String name;
+                    do{
+                        valid = true;
+                        System.out.print("Name: ");
+                        name = scanner.nextLine().strip();
+                        if (name.isBlank()){
+                            System.out.printf("%sName can't be empty%s\n", RED_BOLD_START, RESET);
+                            valid = false;
+                            continue;
+                        }
+                        for (int i = 0; i < name.length(); i++) {
+                            if (!(Character.isLetter(name.charAt(i)) || 
+                                Character.isSpaceChar(name.charAt(i))) ) {
+                                System.out.printf("%sInvalid Name%s\n", RED_BOLD_START, RESET);
+                                valid = false;
+                                break;
+                            }
+                        }
+                    }while(!valid);
+                    String[] newCustName = new String[custName.length + 1];
+                    for (int i = 0; i < custName.length; i++) {
+                        newCustName[i] = custName[i];
+                    }
+                    newCustName[newCustName.length -1] = name;
+                    custName = newCustName;
+
+                    System.out.println();
+                    System.out.print(" " + name + " added sucessfully.\n Do you want to add new student (Y/n)? ");
+                    if (scanner.nextLine().strip().toUpperCase().equals("Y")) continue;
+                    screen = MAIN_MENU;
+                    break;
                 default:
                     break;
             }
-        //} while (true);
+        } while (true);
     }
 }
